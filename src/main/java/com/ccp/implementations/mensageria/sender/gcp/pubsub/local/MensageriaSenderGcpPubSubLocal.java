8e60c9205fe748b8fe0a9213072588681f08a65e
onias <onias@noxxonsat.com.br>
2023-08-30 00:39:44 -0300

    mensagem qualquer

---
+++ src/main/java/com/ccp/implementations/mensageria/sender/gcp/pubsub/local/MensageriaSenderGcpPubSubLocal.java
@@ -0,0 +1,25 @@
+package com.ccp.implementations.mensageria.sender.gcp.pubsub.local;
+
+import com.ccp.constantes.CcpConstants;
+import com.ccp.dependency.injection.CcpDependencyInjection;
+import com.ccp.especifications.http.CcpHttpRequester;
+import com.ccp.especifications.mensageria.sender.CcpMensageriaSender;
+
+public class MensageriaSenderGcpPubSubLocal  implements CcpMensageriaSender {
+
+	@Override
+	public void send(Enum<?> topic, String... msgs) {
+		
+		for (String msg : msgs) {
+			new Thread(() -> this.send(topic, msg)).start();
+		}
+	}
+
+	
+	private void send(Enum<?> topic, String msg) {
+		CcpHttpRequester dependency = CcpDependencyInjection.getDependency(CcpHttpRequester.class);
+		dependency.executeHttpRequest("http://localhost:8081/" + topic.name() + "/testing", "POST", 
+				CcpConstants.EMPTY_JSON, 
+				msg, 200);
+	}
+}
