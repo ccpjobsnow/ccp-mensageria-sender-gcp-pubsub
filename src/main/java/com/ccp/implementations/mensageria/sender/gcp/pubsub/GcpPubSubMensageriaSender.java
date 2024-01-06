c1543965777aae50d5f3cf4d9567eacc8b55cd0a
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-01-06 18:54:32 -0300

    tentativa de integração entre projetos

--- src/main/java/com/ccp/implementations/mensageria/sender/gcp/pubsub/GcpPubSubMensageriaSender.java
+++ src/main/java/com/ccp/implementations/mensageria/sender/gcp/pubsub/GcpPubSubMensageriaSender.java
@@ -1,6 +1,7 @@
 package com.ccp.implementations.mensageria.sender.gcp.pubsub;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Base64;
@@ -8,18 +9,27 @@ import java.util.Base64.Encoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import com.ccp.constantes.CcpConstants;
+import com.ccp.decorators.CcpInputStreamDecorator;
 import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.decorators.CcpStringDecorator;
 import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.especifications.http.CcpHttpHandler;
 import com.ccp.especifications.http.CcpHttpResponseType;
 import com.ccp.especifications.main.authentication.CcpAuthenticationProvider;
 import com.ccp.especifications.mensageria.sender.CcpMensageriaSender;
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.rpc.ApiException;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.pubsub.v1.Publisher;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.protobuf.ByteString;
-import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
 
 class GcpPubSubMensageriaSender implements CcpMensageriaSender {
@@ -28,29 +38,31 @@ class GcpPubSubMensageriaSender implements CcpMensageriaSender {
 
 	private static final Map<String, Publisher> publishers = new HashMap<>();
 
-	private Publisher getPublisher(String topicId) {
-		
-		boolean alreadyCalledBefore = publishers.containsKey(topicId);
-		
-		if(alreadyCalledBefore) {
-			Publisher publisher = publishers.get(topicId);
+	private Publisher getPublisher(String topicName) {
+
+		boolean alreadyCalledBefore = publishers.containsKey(topicName);
+
+		if (alreadyCalledBefore) {
+			Publisher publisher = publishers.get(topicName);
 			return publisher;
 		}
-		
-		ProjectTopicName topico = ProjectTopicName.newBuilder().setProject(PROJECT_ID).setTopic(topicId).build();
 		Publisher publisher = null;
-		
 		try {
-			publisher = Publisher.newBuilder(topico).build();
-			publishers.put(topicId, publisher);
+			CcpStringDecorator ccpStringDecorator = new CcpStringDecorator("GOOGLE_APPLICATION_CREDENTIALS");
+			CcpInputStreamDecorator inputStreamFrom = ccpStringDecorator.inputStreamFrom();
+			InputStream fromEnvironmentVariablesOrClassLoaderOrFile = inputStreamFrom.fromEnvironmentVariablesOrClassLoaderOrFile();
+			GoogleCredentials credentials = GoogleCredentials.fromStream(fromEnvironmentVariablesOrClassLoaderOrFile);
+			FixedCredentialsProvider create = FixedCredentialsProvider.create(credentials);
+			publisher = Publisher.newBuilder(topicName).setCredentialsProvider(create).build();
+			publishers.put(topicName, publisher);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 		return publisher;
 	}
 
-	public void send(Enum<?> topicName, String... msgs) {
-		Publisher publisher = getPublisher(topicName.name()); 
+	public void send2(Enum<?> topicName, String... msgs) {
+		Publisher publisher = getPublisher(topicName.name());
 
 		try {
 			for (String json : msgs) {
@@ -63,20 +75,71 @@ class GcpPubSubMensageriaSender implements CcpMensageriaSender {
 		}
 	}
 
-	public void send1(Enum<?> topicName , String... msgs) {
+	public void send(Enum<?> topicId, String...msgs)
+			{
+		Publisher publisher = getPublisher(topicId.name());
+
+		try {
+
+			List<String> messages = Arrays.asList(msgs);
+
+			for (final String message : messages) {
+				ByteString data = ByteString.copyFromUtf8(message);
+				PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+
+				// Once published, returns a server-assigned message id (unique within the
+				// topic)
+				ApiFuture<String> future = publisher.publish(pubsubMessage);
+
+				// Add an asynchronous callback to handle success / failure
+				ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
+
+					@Override
+					public void onFailure(Throwable throwable) {
+						if (throwable instanceof ApiException) {
+							ApiException apiException = ((ApiException) throwable);
+							// details on the API exception
+							System.out.println(apiException.getStatusCode().getCode());
+							System.out.println(apiException.isRetryable());
+						}
+						System.out.println("Error publishing message : " + message);
+					}
+
+					@Override
+					public void onSuccess(String messageId) {
+						// Once published, returns server-assigned message ids (unique within the topic)
+						System.out.println("Published message ID: " + messageId);
+					}
+				}, MoreExecutors.directExecutor());
+			}
+		} catch(Throwable e) {
+			
+		}
+		finally {
+			if (publisher == null) {
+				return;
+			}
+			try {
+				publisher.shutdown();
+				publisher.awaitTermination(1, TimeUnit.MINUTES);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+
+	public void send1(Enum<?> topicName, String... msgs) {
 		List<String> asList = Arrays.asList(msgs);
-		List<CcpJsonRepresentation> messages = asList.stream().map(message -> this.map(message)).collect(Collectors.toList());
-		String url = "https://pubsub.googleapis.com/v1/projects/"
-				+ PROJECT_ID
-				+ "/topics/"
-				+ topicName
-				+ ":publish";
-		
-		CcpAuthenticationProvider authenticationProvider = CcpDependencyInjection.getDependency(CcpAuthenticationProvider.class);
+		List<CcpJsonRepresentation> messages = asList.stream().map(message -> this.map(message))
+				.collect(Collectors.toList());
+		String url = "https://pubsub.googleapis.com/v1/projects/" + PROJECT_ID + "/topics/" + topicName + ":publish";
+
+		CcpAuthenticationProvider authenticationProvider = CcpDependencyInjection
+				.getDependency(CcpAuthenticationProvider.class);
 		String token = authenticationProvider.getJwtToken();
-		
+
 		CcpJsonRepresentation body = CcpConstants.EMPTY_JSON.put("messages", messages);
-		
+
 		CcpHttpHandler ccpHttpHandler = new CcpHttpHandler(200);
 		CcpJsonRepresentation authorization = CcpConstants.EMPTY_JSON.put("Authorization", "Bearer " + token);
 		ccpHttpHandler.executeHttpRequest(url, "POST", authorization, body, CcpHttpResponseType.singleRecord);
@@ -86,7 +149,7 @@ class GcpPubSubMensageriaSender implements CcpMensageriaSender {
 		Encoder encoder = Base64.getEncoder();
 		byte[] bytes = message.getBytes();
 		byte[] encode = encoder.encode(bytes);
-		String value = new String( encode);
+		String value = new String(encode);
 		CcpJsonRepresentation json = CcpConstants.EMPTY_JSON.put("data", value);
 		return json;
 	}
