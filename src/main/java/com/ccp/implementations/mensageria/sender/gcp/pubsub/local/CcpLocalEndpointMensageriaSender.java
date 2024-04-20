abef66dfa6700eebb44d1044209f3010f90ee677
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:41:39 -0300

    .

--- src/main/java/com/ccp/implementations/mensageria/sender/gcp/pubsub/local/CcpLocalEndpointMensageriaSender.java
+++ src/main/java/com/ccp/implementations/mensageria/sender/gcp/pubsub/local/CcpLocalEndpointMensageriaSender.java
@@ -1,12 +1,11 @@
 package com.ccp.implementations.mensageria.sender.gcp.pubsub.local;
 
 import com.ccp.dependency.injection.CcpInstanceProvider;
+import com.ccp.especifications.mensageria.sender.CcpMensageriaSender;
 
-public class CcpLocalEndpointMensageriaSender implements CcpInstanceProvider {
+public class CcpLocalEndpointMensageriaSender implements CcpInstanceProvider<CcpMensageriaSender> {
 
-	
-	public Object getInstance() {
+	public CcpMensageriaSender getInstance() {
 		return new LocalEndpointMensageriaSender();
 	}
-
 }
