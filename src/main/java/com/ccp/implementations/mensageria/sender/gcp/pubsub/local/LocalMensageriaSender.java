1ca014b83d3efcca62297e57cdc990795bc2f8c8
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-05 21:57:47 -0300

    refatoração da busca de multiget e execução dos 45 cenários de teste para login

--- src/main/java/com/ccp/implementations/mensageria/sender/gcp/pubsub/local/LocalMensageriaSender.java
+++ src/main/java/com/ccp/implementations/mensageria/sender/gcp/pubsub/local/LocalMensageriaSender.java
@@ -1,12 +1,39 @@
 package com.ccp.implementations.mensageria.sender.gcp.pubsub.local;
 
 import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.especifications.mensageria.sender.CcpMensageriaSender;
 import com.ccp.exceptions.process.CcpAsyncTask;
+import com.ccp.implementations.db.bulk.elasticsearch.CcpElasticSerchDbBulk;
+import com.ccp.implementations.db.crud.elasticsearch.CcpElasticSearchCrud;
+import com.ccp.implementations.db.query.elasticsearch.CcpElasticSearchQueryExecutor;
+import com.ccp.implementations.db.utils.elasticsearch.CcpElasticSearchDbRequest;
+import com.ccp.implementations.email.sendgrid.CcpSendGridEmailSender;
+import com.ccp.implementations.file.bucket.gcp.CcpGcpFileBucket;
+import com.ccp.implementations.http.apache.mime.CcpApacheMimeHttp;
+import com.ccp.implementations.instant.messenger.telegram.CcpTelegramInstantMessenger;
+import com.ccp.implementations.json.gson.CcpGsonJsonHandler;
+import com.ccp.jn.async.business.factory.CcpJnAsyncBusinessFactory;
 import com.ccp.jn.async.business.support.JnAsyncBusinessNotifyError;
 
 public class LocalMensageriaSender  implements CcpMensageriaSender {
 
+	public LocalMensageriaSender() {
+		CcpDependencyInjection.loadAllDependencies(
+				new CcpElasticSearchQueryExecutor(),
+				new CcpTelegramInstantMessenger(),
+				new CcpJnAsyncBusinessFactory(),
+				new CcpElasticSearchDbRequest(),
+				new CcpSendGridEmailSender(),
+				new CcpElasticSerchDbBulk(),
+				new CcpElasticSearchCrud(),
+				new CcpGsonJsonHandler(),
+				new CcpApacheMimeHttp(),
+				new CcpGcpFileBucket()  
+			
+	);
+
+	}
 	
 	public void send(String topic, String... msgs) {
 		
